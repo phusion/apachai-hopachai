bf5b6b591824e11b8c88550d9f985f7992423ae1
Hongli Lai (Phusion) <hongli@phusion.nl>
2014-02-17 10:28:22 +0100

    Install Ruby 2.1 on Vagrant

--- vagrant_provision.sh
+++ vagrant_provision.sh
@@ -10,8 +10,13 @@ export DEBIAN_FRONTEND=noninteractive
 
 set -x
 
+if ! [[ -e /etc/apt/sources.list.d/brightbox.list ]]; then
+	apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C3173AA6
+	echo deb http://ppa.launchpad.net/brightbox/ruby-ng/ubuntu precise main > /etc/apt/sources.list.d/brightbox.list
+fi
+
 apt-get update
-apt_get_install ruby1.9.3 ruby1.9.1-dev nodejs runit wget
+apt_get_install ruby2.1 ruby2.1-dev nodejs runit wget
 gem install bundler rake --no-rdoc --no-ri
 
 ln -sf /vagrant/app /appa
