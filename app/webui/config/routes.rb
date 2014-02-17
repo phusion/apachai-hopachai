f2665439b0412dbed3595b1493dd5472754bc6fa
Hongli Lai (Phusion) <hongli@phusion.nl>
2014-02-17 11:55:38 +0100

    Install ActiveAdmin

--- app/webui/config/routes.rb
+++ app/webui/config/routes.rb
@@ -1,4 +1,6 @@
 ApachaiHopachai::Application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "home#index"
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
