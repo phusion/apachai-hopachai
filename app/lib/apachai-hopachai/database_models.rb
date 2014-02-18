b301ac05f0ac5070720b580b76dd163a6361a8bf
Hongli Lai (Phusion) <hongli@phusion.nl>
2014-02-18 17:36:22 +0100

    Allow customizing database information via environment variables.

--- app/lib/apachai-hopachai/database_models.rb
+++ app/lib/apachai-hopachai/database_models.rb
@@ -2,6 +2,7 @@ require 'rails'
 require 'active_record/railtie'
 Bundler.require(:default, :models)
 require 'active_record/migration'
+require 'erb'
 require_relative 'safe_yaml'
 require_relative '../apachai-hopachai'
 
@@ -9,7 +10,8 @@ require_relative '../apachai-hopachai'
 DefaultValueFor.initialize_active_record_extensions
 require "#{ApachaiHopachai::APP_ROOT}/webui/config/initializers/devise"
 
-config = YAML.load_file(ApachaiHopachai::DATABASE_CONFIG_FILE)
+config_data = ERB.new(File.read(ApachaiHopachai::DATABASE_CONFIG_FILE)).result
+config = YAML.load(config_data)
 rails_env = ENV['RAILS_ENV'] || ENV['RACK_ENV'] || 'development'
 ActiveRecord::Base.logger = ApachaiHopachai.default_logger
 ActiveRecord::Base.establish_connection(config[rails_env])
